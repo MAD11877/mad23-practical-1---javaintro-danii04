18b12615d3a91fe9d678d2b80e7c77f0de2027c4
danii04 <92702494+danii04@users.noreply.github.com>
2023-04-20 10:48:47 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -4,6 +4,13 @@ public class Question2
 {
   public static void main(String[] args)
   {
+   height = float(input("Enter your height in meters: "))
+   weight = float(input("Enter your weight in kilograms: "))
+
+# calculate BMI
+bmi = weight / (height ** 2)
+
+print("Your BMI is:", round(bmi, 2))
     /**
      * Prompts user for height and weight, calculate and print out the BMI of the user
      * 
